c87992a98f30bba0a8922607d97c27b46a623185
kleri <NTjwRq6DNTjwRq6D>
2021-02-08 14:00:09 +0200

    Add

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('count starts at 0', () => {
+
+});
+
+test('clicking on button increments counter display', () => {
+
 });
